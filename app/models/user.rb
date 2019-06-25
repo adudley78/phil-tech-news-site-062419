94925a943652e54ddbc4fb6872d37031cb2ac38c
adudley78 <dudleyadam@gmail.com>
2019-06-25 18:44:38 -0400

    Clean up formatting in app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :username, presence :true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
+    validates
+        :username,
+        presence: true,
+        length: { minimum: 3 },
+        uniqueness: { case_sensitive: false }
+   
+    validates
+        :password,
+        length: { minimum: 8 }
 
-    validates :password, length: { minimum: 8 }
+    
 end
